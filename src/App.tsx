126fbcb57f85e1ee97fabcab31d03dca669ff308
vicenteaparicio <miradadesigual@gmail.com>
2024-03-23 18:11:00 +0100

    Change path for home

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/vicenteapi" element={<Home />} />
       </Routes>
     </div>
   );
